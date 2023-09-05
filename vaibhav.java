fe78028cba7a2323788108838bfa615764eed3f0
6677889933 <143704923+6677889933@users.noreply.github.com>
2023-09-05 12:31:02 +0530

    Add files via upload

---
+++ vaibhav.java
@@ -0,0 +1,58 @@
+ package com.arraydemo;
+//array object
+class UserDefinedType
+{
+int i;
+	
+	public void method1()
+	{
+		System.out.println("from user defined type of object");
+	}
+}
+
+
+public class ArrayObjectDemo {
+
+	
+	public static void main(String[] args) {
+		int[] a;
+		int a1[];
+		//UserDefinedType object=new UserDefinedType();// simple object 
+		UserDefinedType[] userDefineArray=new UserDefinedType[10]; // array type of object
+	
+		
+		for(int i=0; i< userDefineArray.length; i++ )
+		{
+		userDefineArray[i]=new UserDefinedType();
+		}
+		userDefineArray[1].method1();
+	}
+
+}
+[5:17 pm, 04/09/2023] +91 98219 02257: package com.arraydemo;
+//array object
+class UserDefinedType
+{
+int i;
+	
+	public void method1()
+	{
+		System.out.println("from user defined type of object");
+	}
+}
+
+
+public class ArrayObjectDemo {
+
+	
+	public static void main(String[] args) {
+		int[] a;
+		int a1[];
+public class vaibhav {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
